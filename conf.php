7aa39e8bede286457fe9fa80bbacb2899daa1118
benir-o <beniromenda@gmail.com>
2025-09-11 20:42:42 +0300

    configuration security

--- conf.php
+++ conf.php
@@ -6,10 +6,10 @@ $conf['admin_email'] = 'benir.omenda@strathmore.edu';
 
 // Database Configuration
 $conf['db_type'] = 'pdo';
-$conf['db_host'] = 'localhost';
-$conf['db_user'] = 'root';
-$conf['db_pass'] = 'ManCity@254';
-$conf['db_name'] = 'basedata5';
+$conf['db_host'] = '';
+$conf['db_user'] = '';
+$conf['db_pass'] = '';
+$conf['db_name'] = '';
 
 // Site Language
 $conf['site_lang'] = 'en';
@@ -18,7 +18,7 @@ $conf['site_lang'] = 'en';
 $conf['mail_type'] = 'smtp'; //Option smtp or mail
 $conf['smtp_host'] = 'smtp.gmail.com';
 $conf['smtp_user'] = 'benir.omenda@strathmore.edu';
-$conf['smtp_pass'] = 'jvvw ivdm buye uauw';
+$conf['smtp_pass'] = '';
 $conf['smtp_port'] = 465;
 $conf['smtp_secure'] = 'ssl';
 
