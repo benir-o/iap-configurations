ac4585d5c10abcd7f6dc8157c8ab501f013aaec3
benir-o <beniromenda@gmail.com>
2025-09-13 16:41:32 +0300

    using bootstrap to design layout and improve functionality

--- Forms/forms.php
+++ Forms/forms.php
@@ -6,15 +6,22 @@ class forms
 ?>
 
         <form method="post" action="/iap-configurations/Global/register.php">
-            <label for="username">Username: </label>
-            <input type="text" id="username" placeholder="John Smith" name="username">
-            <br>
-            <label for="email">Email: </label>
-            <input type="email" id="email" required name="email">
-            <br>
-            <label for="password">Password: </label>
-            <input type="password" id="password" name="password">
-            <button type="submit">Register</button>
+            <div class="container-md bg-light rounded-4 p-4 my-5 shadow">
+                <label for="username">Username:</label>
+                <input type="text" id="username" class="form-control" placeholder="John Smith" aria-label="Username" aria-describedby="basic-addon1">
+                <!--<input type="text" id="username" placeholder="John Smith" name="username">-->
+                <br>
+                <label for="email">Email: </label>
+                <input type="text" id="email" class="form-control" required aria-label="Username" aria-describedby="basic-addon1">
+                <!--<input type="email" id="email" required name="email">-->
+                <br>
+                <label for="password">Password: </label>
+                <input type="text" id="password" class="form-control" required aria-label="Username" aria-describedby="basic-addon1">
+                <br>
+                <!--<input type="password" id="password" name="password">-->
+
+                <button type="button" class="btn btn-primary">Register</button>
+            </div>
         </form>
         <br>
 
--- Layouts/layouts.php
+++ Layouts/layouts.php
@@ -4,18 +4,29 @@ class Layouts
     public function header($conf)
     {
 ?>
-        <header>
-            <h1>Welcome to <?php print $conf['site_name']; ?></h1>
-        </header>
+
+        <head>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <div class="container-md bg-light rounded-4 p-4 my-5 shadow">
+            <header>
+                <h1>Welcome to <?php print $conf['site_name']; ?></h1>
+            </header>
+        </div>
+
     <?php
     }
 
     public function footer($conf)
     {
     ?>
-        <footer>
-            <p>Copyright &copy; <?php echo date("Y"); ?> <?php print $conf['site_name']; ?> - All rights Reserved</p>
-        </footer>
+        <div class="container-md bg-light rounded-4 p-4 my-5 shadow">
+            <footer>
+                <p>Copyright &copy; <?php echo date("Y"); ?> <?php print $conf['site_name']; ?> - All rights Reserved</p>
+            </footer>
+        </div>
+
 <?php
     }
 }
