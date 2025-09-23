a7ce1cee343a9d0b5708355e5e02b8cd655ccb88
benir-o <beniromenda@gmail.com>
2025-09-23 19:09:54 +0300

    Linear search for Authors by Name

--- Global/bookAuthors.php
+++ Global/bookAuthors.php
@@ -1,13 +1,33 @@
 <?php
 class Authors
 {
-    public String $author_name;
-    public String $books = [];
+    public $author_name;
+    public $books = [];
+    public static $authorCollection = [];
 
-    function __construct($author_name, $author_books)
+    function __construct($author_name, $books = [])
     {
         $this->author_name = $author_name;
-        $this->books = $author_books;
+        $this->books = $books;
+        self::$authorCollection[$author_name] = $this->books;
+    }
+    public static function displayAllAuthorsandBooks()
+    {
+        foreach (self::$authorCollection as $bookAuthor => $bookList) {
+            echo "Author: " . $bookAuthor . "\nBooks: ";
+            foreach ($bookList as $bookName) {
+                echo " " . $bookName;
+            }
+            echo "\n";
+        }
+    }
+    public static function getAuthorBooks($author_name)
+    {
+        //A ternary operation that checks if the author is within the dataset
+        return isset(self::$authorCollection[$author_name]) ? self::$authorCollection[$author_name] : null;
     }
 }
 $author1 = new Authors("James Clear", ['Atomic Habits', 'Made it Last Year']);
+$author2 = new Authors("Morgan Housel", ["The Undoing Project", "The Psychology of Money"]);
+
+Authors::displayAllAuthorsandBooks();
